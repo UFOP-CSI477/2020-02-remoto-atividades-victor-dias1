7a71fbccf6fc3a5f20bf9b2103a616346457a2ba
victor-dias1 <85907795+victor-dias1@users.noreply.github.com>
2021-07-29 14:03:54 -0300

    Add files via upload

---
+++ Atividades/exercicios/exercicio-05/cadastro/validar.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "POST";
+var_dump($_POST);
+
+$name = $_POST['nome'];
+$address = $_POST['endereco'];
+$id = $_POST['cpf'];
+$fone = $_POST['tel'];
+
+
+echo ("<h1>Olá, $name!</>");
+echo ("<h2>Seja bem-vindx!</h2>");
+echo ("<p>CPF: $id</p>");
+echo ("<p>Contato: $fone</p>");
+echo ('<a href="01-prompt.html">Voltar</a>');
