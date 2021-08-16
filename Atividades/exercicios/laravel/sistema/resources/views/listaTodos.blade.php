6f5fbdca11a9c06ba27d553ab283c6d53bfcfd8b
Dias-Victor <victor.dias1@aluno.ufop.edu.br>
2021-08-16 16:06:20 -0300

    lista dados

---
+++ Atividades/exercicios/laravel/sistema/resources/views/listaTodos.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <title>Lista</title>
+</head>
+
+<body>
+    <div class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Produto</th>
+                    <th scope="col">Unidade de Medida</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($dados as $dado)
+                <tr>
+                    <th>{{ $dado->id }}</th>
+                    <td>{{ $dado->nome }}</td>
+                    <td>{{ $dado->um }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+</body>
+
+</html>
